85be993da61d09a730323d256e8131c8b8768c31
psurreaux <surreauxpp@gmail.com>
2024-11-08 22:49:55 -0300

    Commit inicial do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projeto Movie Plots
+
+## Projeto
+Serviço para integração de API de filmes para tradução de sinopses de filmes para português.
+
+## APIs utilizadas
+- API com a database de filmes http://www.omdbapi.com/
+- Serviço de tradução https://github.com/LibreTranslate/LibreTranslate
+
+## Stack
+- JavaScript
+- Express
+- Docker
---
+++ src/controllers/MovieController.js
@@ -0,0 +1,19 @@
+import MovieService from "../services/MovieService.js";
+import { formatMovieObject } from "../utils/helpers.js";
+
+class MovieController {
+  static async getMoviePlot (req, res) {
+    const movieName = req.query.movie;
+    try {
+      const moviePlot = await MovieService.getMovieInfo(movieName);
+      const response = formatMovieObject(moviePlot);
+      const translatedPlot = await MovieService.getTranslation(response);
+
+      res.status(200).json(translatedPlot); 
+    } catch (error) {
+      res.status(500).json({ message: error.message });  
+    }
+  }  
+}
+
+export default MovieController;
